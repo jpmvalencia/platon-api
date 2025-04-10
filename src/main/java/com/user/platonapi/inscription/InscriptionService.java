378978064929b0ea50ac7757f9b51f1854fec5d9
Juan Pablo Méndez Valencia <juan_mendez23231@elpoli.edu.co>
2025-04-09 19:49:21 -0500

    feat(inscription): add inscription validation and HTTP handling in `InscriptionService` and `InscriptionServlet`
    - Implemented `addInscription` method in `InscriptionService` to add new inscriptions with validation checks for duplicate students in a subject and a limit of 7 subjects per student.
    - Updated `InscriptionServlet` to handle HTTP POST requests for creating new inscriptions and send appropriate success or error responses.
    - Added `addInscription`, `existsStudentInSubject`, and `getInscriptionsByStudent` methods in `InscriptionDAO` for managing inscription data.

--- src/main/java/com/user/platonapi/inscription/InscriptionService.java
+++ src/main/java/com/user/platonapi/inscription/InscriptionService.java
@@ -1,11 +1,44 @@
 package com.user.platonapi.inscription;
 
+import com.user.platonapi.common.response.SuccessResponse;
 import com.user.platonapi.inscription.dao.InscriptionDAO;
+import com.user.platonapi.inscription.dto.InscriptionDTO;
+import com.user.platonapi.inscription.model.Inscription;
+
+import java.io.IOException;
+import java.util.List;
 
 public class InscriptionService {
     private final InscriptionDAO inscriptionDAO;
 
     public InscriptionService(InscriptionDAO inscriptionDAO) {
         this.inscriptionDAO = inscriptionDAO;
+    }
+
+    public SuccessResponse<Inscription> addInscription(InscriptionDTO inscriptionDTO) throws Exception {
+        Inscription inscription = new Inscription(
+                inscriptionDTO.getStudent(),
+                inscriptionDTO.getDocument(),
+                inscriptionDTO.getCareer(),
+                inscriptionDTO.getSubject(),
+                inscriptionDTO.getSemester(),
+                inscriptionDTO.getEnrollmentDate(),
+                inscriptionDTO.getStatus()
+        );
+
+        if (inscriptionDAO.existsStudentInSubject(inscription.getStudent(), inscription.getSubject())) {
+            throw new Exception("student already in subject");
+        }
+        if (inscriptionDAO.getInscriptionsByStudent(inscription.getStudent()).size() >= 7) {
+            throw new Exception("student is already in 7 subjects");
+        }
+
+        inscriptionDAO.addInscription(inscription);
+
+        return new SuccessResponse(
+                "inscription created successfully",
+                201,
+                inscription
+        );
     }
 }
--- src/main/java/com/user/platonapi/inscription/dao/InscriptionDAO.java
+++ src/main/java/com/user/platonapi/inscription/dao/InscriptionDAO.java
@@ -8,4 +8,28 @@ import java.util.List;
 public class InscriptionDAO {
     private List<Inscription> inscriptions = new ArrayList<>();
     private static int idCounter = 1;
+
+    public void addInscription(Inscription inscription) {
+        inscription.setId(idCounter++);
+        inscriptions.add(inscription);
+    }
+
+    public boolean existsStudentInSubject(String student, String subject) {
+        for (Inscription inscription : inscriptions) {
+            if (inscription.getStudent().equals(student) && inscription.getSubject().equals(subject)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<Inscription> getInscriptionsByStudent(String student) {
+        List<Inscription> studentInscriptions = new ArrayList<>();
+        for (Inscription inscription : this.inscriptions) {
+            if (inscription.getStudent().equals(student)) {
+                studentInscriptions.add(inscription);
+            }
+        }
+        return studentInscriptions;
+    }
 }
